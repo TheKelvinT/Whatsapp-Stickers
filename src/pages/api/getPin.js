6d67f8ca6db2aeedf29d8b622ecd84fe8ebe4370
Kelvin Tan <100819164+TheKelvinT@users.noreply.github.com>
2023-03-10 01:17:43 +0800

    updated 3 tries

--- src/pages/api/getPin.js
+++ src/pages/api/getPin.js
@@ -1,25 +1,33 @@
 async function getPin(phoneNumber, userId) {
-  // Check if phone number is between 8 and 12 characters long
-  if (phoneNumber.length < 10 || phoneNumber.length > 12) {
+  // Check if phone number is valid
+  if (phoneNumber.length < 10 || phoneNumber.length > 13) {
     return {
       success: false,
-      error: "Phone number must be atleast 10 to 12 characters long",
+      error: "Invalid Phone Number",
     };
   }
 
   //URL with query parameters
   const url = `https://75j6v3-8080.preview.csb.app/api/v1/trigger-pin?msisdn=${phoneNumber}&user_id=${userId}`;
 
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      return { success: true, data: data };
-    } else {
-      throw new Error("Network response was not ok");
+  //Trigger 3 tries before throw an error
+  let tries = 3;
+
+  while (tries > 0) {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        return { success: true, data: data };
+      } else {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      tries--;
+      if (tries === 0) {
+        return { success: false, error: error };
+      }
     }
-  } catch (error) {
-    return { success: false, error: error };
   }
 }
 
